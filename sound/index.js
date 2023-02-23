8475dd671b8814af661166648b7bc8e059dba92b
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-02-23 15:44:17 +1000

    Sound

--- sound/index.js
+++
@@ -1,7 +0,0 @@
-const titleStorage = localStorage.getItem("title");
-const iconStorage = localStorage.getItem("icon");
-
-const favicon = document.getElementById("favicon");
-
-document.title = titleStorage ? titleStorage : "NVSHS";
-favicon.href = iconStorage;
