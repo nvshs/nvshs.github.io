5fe5107b0c9f844895e980438fcb019533931ba4
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-02-26 10:56:21 +1000

    Light and dark

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const titleStorage = localStorage.getItem("title");
 const iconStorage = localStorage.getItem("icon");
+const colourStorage = localStorage.getItem("colour");
 
 const favicon = document.getElementById("favicon");
 
 document.title = titleStorage ? titleStorage : "NVSHS";
 favicon.href = iconStorage;
+
+document.documentElement.style.overflow = "hidden";
+document.body.clientWidth;
+document.documentElement.style.overflow = "";
+if (colourStorage == 0) {
+    document.documentElement.setAttribute("data-color-scheme", "dark");
+}
+if (colourStorage == 1) {
+    document.documentElement.setAttribute("data-color-scheme", "light");
+}
--- settings/index.js
+++ settings/index.js
@@ -1,11 +1,10 @@
-const titleInput = document.getElementById("title").value;
-const iconInput = document.getElementById("icon").value;
-
 function setTitle() {
+    const titleInput = document.getElementById("title").value;
     localStorage.setItem("title", titleInput);
     document.title = titleInput;
 }
 function setIcon() {
+    const iconInput = document.getElementById("icon").value;
     localStorage.setItem("icon", iconInput);
     document.getElementById("favicon").href = iconInput;
 }
@@ -14,4 +13,18 @@ function reset() {
     localStorage.removeItem("icon");
     document.getElementById("favicon").href = null;
     document.title = "NVSHS";
+}
+
+function setColour() {
+    const colourInput = document.getElementById("colour").value;
+    localStorage.setItem("colour", colourInput);
+    document.documentElement.style.overflow = "hidden";
+    document.body.clientWidth;
+    document.documentElement.style.overflow = "";
+    if (colourInput == 0) {
+        document.documentElement.setAttribute("data-color-scheme", "dark");
+    }
+    if (colourInput == 1) {
+        document.documentElement.setAttribute("data-color-scheme", "light");
+    }
 }
